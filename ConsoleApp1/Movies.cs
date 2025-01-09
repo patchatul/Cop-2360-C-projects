7240d20d27f4bb5c8cbb86549bbe4e74fb9c5205
Pat Tul <patchatulyakul@gmail.com>
2025-01-09 14:52:51 -0500

    A1 Movies Console

---
+++ ConsoleApp1/Movies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Movies
+{
+    static void Main()
+    {
+        Console.WriteLine("My favorite movies: ");
+        Console.WriteLine("1. WALL-E");
+        Console.WriteLine("2. Decision To Leave");
+        Console.WriteLine("3. Little Women");
+        Console.WriteLine("4. Easy A");
+    }
+}
