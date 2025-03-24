e14b8b0089829fc397fda307ea7e0bd4fafd1d8b
Pat Tul <patchatulyakul@gmail.com>
2025-03-24 16:27:50 -0400

    A9 Class Get set Instance Object

--- ConsoleApp1/RRHOF.cs
+++ ConsoleApp1/RRHOF.cs
@@ -9,25 +9,24 @@ class RRHOF
         Inductees inductee2 = new Inductees("JANET JACKSON", 2019);
         Inductees inductee3 = new Inductees("CHER", 2024);
         
-        inductee1.Output();
-        inductee2.Output();
-        inductee3.Output();
+        inductee1.OutputMessage();
+        inductee2.OutputMessage();
+        inductee3.OutputMessage();
     }
 }
 class Inductees
     {
-        public string Artist
-        { get; set;}
-        public int Year
-        { get; set;}
+        public string Artist { get; set;}
+        public int Year { get; set;}
         public Inductees(string artist, int year)
         {
             Artist = artist;
             Year = year;
         }
-        public void Output()
+        public void OutputMessage()
         {
             WriteLine("The artist is " + Artist);
             WriteLine("The year of induction is " + Year);
         }
+    }
     }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa698d3bf32c1b6a56ef069e3c30f886919ae57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db067eb1eeb32694d6ef6cc7d3d18d455b9382e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
